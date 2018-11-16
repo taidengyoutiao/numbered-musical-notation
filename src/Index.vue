dc42ead9e105f326ad75a68d68ffc81596559b26
Leo <yinfu1358@163.com>
2018-11-16 17:38:56 +0800

    主页添加记谱说明

--- src/Index.vue
+++ src/Index.vue
@@ -1,11 +1,34 @@
 <template>
-  <div class="wrapper">
-    <h2 class="title">{{title}}</h2>
-    <div>
-      <Bar v-for="(item, key) in notes" :key="key" :barData="item"></Bar>
+  <div>
+    <div class="wrapper">
+      <h2 class="title">{{title}}</h2>
+      <div>
+        <Bar v-for="(item, key) in notes" :key="key" :barData="item"></Bar>
+      </div>
+      <div>
+        <textarea class="content-inp" v-model="content"></textarea>
+      </div>
     </div>
-    <div>
-      <textarea class="content-inp" v-model="content"></textarea>
+    <div class="info">
+      <h2>记谱法：</h2>
+      <h3>音高</h3>
+      <ul>
+        <li>0-7分别代表休止符和音高</li>
+        <li>^$ 代表高音低音，支持倍高音和倍低音</li>
+        <li>#1 @1 代表升1和降1</li>
+      </ul>
+      <h3>节奏</h3>
+      <ul>
+        <li>十六分音符为最小单位</li>
+        <li>qwe 分别代表四分音符、八分音符、十六分音符</li>
+        <li>qw 代表带附点的四分音符，其他以此类推</li>
+        <li>qq qqqq 代表二分音符、全音符</li>
+      </ul>
+      <h3>其他</h3>
+      <ul>
+        <li>逗号用来区分音符</li>
+        <li>空格用来区分小节</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -24,7 +47,9 @@ export default {
         w: 2,
         e: 1,
         qw: 6,
-        we: 3
+        wq: 6,
+        we: 3,
+        ew: 3
       }
     }
   },
@@ -34,7 +59,7 @@ export default {
       let content = this.content
       let arr = []
       content = content.replace(/\r|\n/g, ' ')
-      content = content.replace(/\s+/g, ' ')
+      content = content.replace(/\s+/g, ' ').trim()
       content = content.split(' ')
       for (let bar of content) {
         let barArr = []
@@ -73,12 +98,16 @@ export default {
 </script>
 
 <style scoped>
-  .wrapper {
+  .wrapper, .info {
     width: 960px;
     background-color: #fff;
     box-shadow: 0px 0px 10px #ccc;
     margin: 0 auto;
     padding: 30px;
+  }
+  .info {
+    background-color: #e1e1e1;
+    color: #333;
   }
   .title {
     text-align: center;
