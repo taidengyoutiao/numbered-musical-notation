57a9ac62ca51b8f8b6733f4ec7b8481a100e5dcf
April <yinfu1358@163.com>
2025-09-16 18:28:28 +0800

    feat: expand twinkle twinkle variations

--- src/test.js
+++ src/test.js
@@ -1,10 +1,20 @@
 export default {
-  title: 'twinkle twinkle little star',
+  title: '小星星 - 莫扎特主题与变奏（节选）',
   content: `
-    1q,1q,5q,5q, 6q,6q,5qq, 4q,4q,3q,3q, 2we,3e,2we,3e,1qq
+    1q,1q,5q,5q  6q,6q,5qq  4q,4q,3q,3q  2q,2q,1qq
+    5q,5q,4q,4q  3q,3q,2qq  5q,5q,4q,4q  3q,3q,2qq
+    1q,1q,5q,5q  6q,6q,5qq  4q,4q,3q,3q  2q,2q,1qq
 
-    5q,5q,4q,4q, 3q,3q,2qq, 5q,5q,4q,4q, 3w,4w,3w,4w,2qq
+    0qq,0qq
 
-    1q,1q,5q,5q, 6q,6q,5qq, 4q,4q,3q,3q, 2q,2q,1qq
+    1e,2e,3e,1e,1e,2e,3e,1e,5e,6e,7e,5e,5e,6e,7e,5e  6e,7e,^1e,6e,6e,7e,^1e,6e,5e,6e,7e,5e,5e,6e,7e,5e  4e,5e,6e,4e,4e,5e,6e,4e,3e,4e,5e,3e,3e,4e,5e,3e  2e,3e,4e,2e,2e,3e,4e,2e,1e,2e,3e,1e,1e,2e,3e,1e
+    5e,6e,7e,5e,5e,6e,7e,5e,4e,5e,6e,4e,4e,5e,6e,4e  3e,4e,5e,3e,3e,4e,5e,3e,2e,3e,4e,2e,2e,3e,4e,2e  5e,6e,7e,5e,5e,6e,7e,5e,4e,5e,6e,4e,4e,5e,6e,4e  3e,4e,5e,3e,3e,4e,5e,3e,2e,3e,4e,2e,2e,3e,4e,2e
+    1e,2e,3e,1e,1e,2e,3e,1e,5e,6e,7e,5e,5e,6e,7e,5e  6e,7e,^1e,6e,6e,7e,^1e,6e,5e,6e,7e,5e,5e,6e,7e,5e  4e,5e,6e,4e,4e,5e,6e,4e,3e,4e,5e,3e,3e,4e,5e,3e  2e,3e,4e,2e,2e,3e,4e,2e,1e,2e,3e,1e,1e,2e,3e,1e
+
+    0qq,0qq
+
+    1w,3w,1w,3w,5w,7w,5w,7w  6w,^1w,6w,^1w,5w,7w,5w,7w  4w,6w,4w,6w,3w,5w,3w,5w  2w,4w,2w,4w,1w,3w,1w,3w
+    5w,7w,5w,7w,4w,6w,4w,6w  3w,5w,3w,5w,2w,4w,2w,4w  5w,7w,5w,7w,4w,6w,4w,6w  3w,5w,3w,5w,2w,4w,2w,4w
+    1w,3w,1w,3w,5w,7w,5w,7w  6w,^1w,6w,^1w,5w,7w,5w,7w  4w,6w,4w,6w,3w,5w,3w,5w  2w,4w,2w,4w,1w,3w,1w,3w
   `
 }
